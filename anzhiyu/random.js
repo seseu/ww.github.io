99e39323d8e2f2bc8af6d3c06cfe730100359203
seseu <459474035@qq.com>
2023-11-10 01:22:53 +0800

    Site updated: 2023-11-10 01:22:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/10/27/article-title/","2023/11/09/hello-world/"];function toRandomPost(){
+var posts=["2023/11/09/hello-world/","2023/10/27/article-title/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
